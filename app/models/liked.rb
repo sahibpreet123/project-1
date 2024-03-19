e957c97aa935598e89661b7e58651c279ebaeab6
Sahibpreet123 <lubanajot131@gmail.com>
2024-03-19 15:21:06 -0500

    added belongings

--- app/models/liked.rb
+++ app/models/liked.rb
@@ -3,6 +3,9 @@ class Liked < ApplicationRecord
   belongs_to :user
 
   validates :character, presence: true
+
+
   validates :user, presence: true
+  
   validates :character_id, uniqueness: { scope: :user_id, message: "has already been liked by this user" }
 end
