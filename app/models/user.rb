15aa2ccab6ce7cf264645e7a241235fc344e2e6c
Sahibpreet123 <lubanajot131@gmail.com>
2024-03-19 15:28:21 -0500

    users file corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :likes
   has_many :characters, through: :likes
 
+
   validates :email, presence: true, uniqueness: true
+  
   validates :name, presence: true
 end
